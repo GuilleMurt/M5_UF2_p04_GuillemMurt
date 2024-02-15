536f0dce3e50b8f1e7a1d6f4180c20a798f39a5f
GuilleMurt <magicguillem.gmail.com>
2024-02-15 20:54:42 +0100

    a

--- operaciones/index.js
+++ operaciones/index.js
@@ -5,11 +5,7 @@
  * @returns {Number} el resultat de la operacio
  */
 function suma(...valors) {
-  let number = 0;
-  for (let valor of numbers) {
-    number += valor;
-  }
-  return number;
+
 }
 
 /**
@@ -19,11 +15,7 @@ function suma(...valors) {
  * @returns {Number} el resultat de la operacio
  */
 function multiplica(...valors) {
-  let number = 1;
-  for (let valor of numbers) {
-    number *= valor;
-  }
-  return number;
+
 }
 
 module.exports.suma = suma;
